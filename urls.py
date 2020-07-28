abca7d673c9ce3dbbe530e855fd0f5110c96ce1c
Mohamed Radwan <mohamed.radwan@cartologic.com>
2020-07-28 17:50:59 +0200

    Upgrade to Python 3 and Django 2.2

--- urls.py
+++ urls.py
@@ -5,11 +5,10 @@
 # urlpatterns = patterns('',
 #     url(r'^$', views.index, name='%s.index' % APP_NAME),
 # )
-from django.conf.urls import url, include
-import views
-from . import APP_NAME
+from django.urls import path, re_path, include
+from . import views, APP_NAME
 
-from api import LayerResource
+from .api import LayerResource
 from tastypie.api import Api
 
 
@@ -18,12 +17,9 @@ Resources_api.register(LayerResource())
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='%s.index' % APP_NAME),
-    url(r'^styles/(?P<layername>[^/]*)$',
-        views.layer_styles, name='%s.layer_styles' % APP_NAME),
-    url(r'^styles/save/(?P<layer_name>[^/]*)/(?P<style_name>[^/]*)$',
-        views.save_style, name='%s.save_style' % APP_NAME),
-    url(r'^proxy/geoserver/rest/(?P<suburl>.*)$',
-        views.geoserver_rest_proxy, name='%s.proxy' % APP_NAME),
-    url(r'^', include(Resources_api.urls)),
+    re_path(r'^$', views.index, name='%s.index' % APP_NAME),
+    path('styles/<str:layername>/', views.layer_styles, name='%s.layer_styles' % APP_NAME),
+    path('styles/save/<str:layer_name>/<str:style_name>/', views.save_style, name='%s.save_style' % APP_NAME),
+    re_path(r'^proxy/geoserver/rest/(?P<suburl>.*)$', views.geoserver_rest_proxy, name='%s.proxy' % APP_NAME),
+    re_path(r'^', include(Resources_api.urls)),
 ]
